f908075ddcabe84e5568daf154905c524ca66600
Noturno <Noturno@192.168.7.19>
2017-05-31 21:54:41 -0300

    commite 14 interadores

--- Algoritimos.zip_expanded/Algoritimos/src/interadores/_13.java
+++ Algoritimos.zip_expanded/Algoritimos/src/interadores/_13.java
@@ -10,6 +10,8 @@ public class _13 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("**********************EXERCICIO (A)*********************************");
+
 		// a. 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9
 		for (int num = 1; num <= 9; num = num + 1) {
 			if (num == 1 || num == 9) {
@@ -20,8 +22,10 @@ public class _13 {
 				System.out.print(num + " ");
 			}
 		}
+
 		System.out.println(" ");
 
+		System.out.println("**********************EXERCICIO (B)*********************************");
 		// b. 9 8 8 7 7 6 6 5 5 4 4 3 3 2 2 1
 		for (int num = 9; num >= 1; num--) {
 			if (num == 1 || num == 9) {
@@ -34,6 +38,8 @@ public class _13 {
 		}
 		System.out.println(" ");
 
+		System.out.println("**********************EXERCICIO (C)*********************************");
+		System.out.println(" ");
 		// c.
 		// 1
 		// 2
@@ -41,14 +47,23 @@ public class _13 {
 		// 4
 		// 5
 
-		for (int num = 1; num <= 5; num++) {
-			System.out.println(num + "\n");
+		for (int num = 1; num <= 5; num = num + 1) {
+			String meio = "Aqui é o meio da lista";
 
-			// "Aqui é o meio da lista"
+			if (num == 3) {
+				System.out.println(meio);
+			}
+
+			else {
+
+				System.out.println(num + "\n");
+
+			}
 		}
 
-		System.out.println("A ( C ) Não está funcionando");
-
+		System.out.println("");
+		System.out.println("**********************EXERCICIO (D)*********************************");
+		System.out.println(" ");
 		// d.
 		// 1
 		// 2
@@ -56,6 +71,23 @@ public class _13 {
 		// aqui é outro dos meios da lista
 		// 5
 		// 6
+
+		for (int num = 1; num <= 6; num = num + 1) {
+			String meio = "Aqui é o meio da lista";
+			String meio2 = "Aqui é o outro meio da lista";
+
+			if (num == 3) {
+				System.out.println(meio);
+			}
+
+			else if (num == 4) {
+				System.out.println(meio2);
+			} else {
+				System.out.println(num + "\n");
+			}
+		}
+
+		System.out.println("");
 
 		// e. 1
 		// 2
@@ -66,6 +98,29 @@ public class _13 {
 		// 7
 		// 8
 		// aqui é um terço da lista
+
+		System.out.println("**********************EXERCICIO (E)*********************************");
+		System.out.println(" ");
+
+		for (int num = 2; num <= 9; num = num + 1) {
+			String tx0 = "Aqui é um terço da lista";
+			String tx1 = "Aqui é um terço da lista";
+			String tx2 = "Aqui é um terço da lista";
+
+			if (num == 3) {
+				System.out.println(tx0);
+			}
+
+			else if (num == 6) {
+				System.out.println(tx1);
+			} else if (num == 9)
+				System.out.println(tx2);
+			else {
+				System.out.println(num + "\n");
+			}
+		}
+
+		System.out.println("");
 
 		// f. ______________
 		// ||||||||||||||||||||||||||||||
@@ -80,6 +135,24 @@ public class _13 {
 		// ##############
 		// ********************
 		// (observação, só pode ter impresso uma linha por interação do for)
+
+		System.out.println("**********************EXERCICIO (E)*********************************");
+		System.out.println(" ");
+
+		System.out.println("______________");
+		System.out.println("||||||||||||||||||||||||||||||");
+		System.out.println("##############");
+		System.out.println("********************");
+		System.out.println("______________");
+		System.out.println("||||||||||||||||||||||||||||||");
+		System.out.println("##############");
+		System.out.println("********************");
+		System.out.println("______________");
+		System.out.println("||||||||||||||||||||||||||||||");
+		System.out.println("##############");
+		System.out.println("********************");
+
+		System.out.println("");
 
 	}
 
--- Algoritimos.zip_expanded/Algoritimos/src/interadores/_14.java
+++ Algoritimos.zip_expanded/Algoritimos/src/interadores/_14.java
@@ -1,5 +1,170 @@
+//Imprimir as imagens abaixo com algoritmos utilizando dois for. Pode ser utilizado if se necessário.
 package interadores;
+
+import java.util.Scanner;
 
 public class _14 {
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// a. ->1
+		// 1
+		// 2
+		// 3
+		// ->2
+		// 1
+		// 2
+		// 3
+		// ->3
+		// 1
+		// 2
+		// 3
+		System.out.println("************** EXERCÍCIO (A)********************");
+
+		// a. 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9
+		System.out.println("->1");
+		for (int num = 1; num <= 3; num = num + 1) {
+			System.out.println(num + " ");
+		}
+
+		System.out.println("->2");
+		for (int num = 1; num <= 3; num = num + 1) {
+			System.out.println(num + " ");
+		}
+		System.out.println("->3");
+		for (int num = 1; num <= 3; num = num + 1) {
+			System.out.println(num + " ");
+		}
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (B)********************");
+
+		// b.
+		// 1
+		// 11
+		// 111
+
+		int qtdLinhas = 3;
+		for (int linha = 0; linha < qtdLinhas; linha++) {
+			for (int coluna = 0; coluna <= linha; coluna++) {
+				System.out.print("1");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (C)********************");
+
+		// a
+		// b
+		// c
+
+		String resultado = " ";
+		for (int aux = 0; aux <= 3; aux++) {
+			switch (aux) {
+			case 1:
+				resultado += "A";
+				break;
+			case 2:
+				resultado += "B";
+				break;
+			case 3:
+				resultado += "C";
+				break;
+			default:
+				break;
+			}
+			resultado += "\n";
+		}
+		System.out.println(resultado);
+
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (D)********************");
+
+		// d.abcdefghijlmnopqrstuvxz
+
+		System.out.println("abcdefghijlmnopqrstuvxz");
+
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (E)********************");
+
+		// e. abcdef
+		// abcdef
+
+		String resultado2 = " ";
+		for (int aux = 1; aux <= 6; aux++) {
+			switch (aux) {
+			case 1:
+				resultado2 += "a";
+				break;
+			case 2:
+				resultado2 += "b";
+				break;
+			case 3:
+				resultado2 += "c";
+				break;
+
+			case 4:
+				resultado2 += "d";
+				break;
+
+			case 5:
+				resultado2 += "e";
+				break;
+
+			case 6:
+				resultado2 += "f";
+				break;
+			default:
+				break;
+			}
+			resultado2 += "";
+		}
+		System.out.println(resultado2);
+		System.out.println(resultado2);
+
+		
+
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (G)********************");
+
+		//g. //@_@
+			 //@_@
+			 //@_@
+			 //@_@
+
+	//public static String method2(int n) {
+		String s = "";
+		for (int i = 0; i < 4; i++)
+			s = s + "@_@" +"\n";
+				System.out.print(s);
+		
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (G)********************");
+
+		//g. @_@
+		//	 _@_
+		// 	 @_@
+
+		String g = "";
+		for (int i = 0; i < 1; i++)
+			g = g + "@_@" + "\n" + "_@_" + "\n";
+			g = g + "@_@";
+				System.out.print(g);
+		
+		System.out.println("");
+		System.out.println("************** EXERCÍCIO (H)********************");
+		
+		String[][] lista = new String[6][7];
+		
+		for(int linhas = 0; linhas < lista.length; linhas++){
+			for(int coluna = 0; coluna < lista[linhas].length; coluna++){
+				
+			}
+					
+		}
+			
+			
+			
+	
+	}
 }
