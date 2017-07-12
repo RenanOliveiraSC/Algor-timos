4fb4582c90d5bfb5dd77f6156a71350ee06b9af8
Renan <Renan@SAPDELL>
2017-07-12 10:23:40 -0300

    commit

--- Algoritimos.zip_expanded/Algoritimos/src/_07.java
+++ Algoritimos.zip_expanded/Algoritimos/src/_07.java
@@ -4,14 +4,10 @@ public class _07 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		String fabricante = "Sony";
 		int volume = 0;
 		boolean ligado = false;
-		
-		public void aumentarVolume(){
-			volume++;
-		}
-		
+
 	}
 }
--- Algoritimos.zip_expanded/Algoritimos/src/_11_while.java
+++ Algoritimos.zip_expanded/Algoritimos/src/_11_while.java
@@ -10,22 +10,14 @@ public class _11_while {
 			System.out.println(count);
 			count++;
 		}
-		
-	 
-		int inicial=1, 
-		razao=3, 
-		an=inicial, 
-		valor_max=20; 
-		System.out.printf("Elementos da PA, de valor inicial %d e razão %d, menores que %d\n", inicial, razao, valor_max ); 
-		while(an<=valor_max){ 
-		System.out.println(an); 
-		an += razao;
-		
-		        } 
-		    } 
 
-		
-		
-		
+		int inicial = 1, razao = 3, an = inicial, valor_max = 20;
+		System.out.printf("Elementos da PA, de valor inicial %d e razão %d, menores que %d\n", inicial, razao,
+				valor_max);
+		while (an <= valor_max) {
+			System.out.println(an);
+			an += razao;
+
+		}
 	}
 }
